1e1e44670715ac242b784ad9f57e17ad0a754832
Weichen Tie <weichentie@gmail.com>
2025-04-05 14:28:40 +1100

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,32 @@
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+
+map("n", "<leader>pv", vim.cmd.Ex)
+map("n", "<leader>rrr", ":luafile ~/.config/nvim/lua/config/keymaps.lua<CR>", { desc = "Reload keymaps" })
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+map("n", "<C-d>", "<C-d>zz", { desc = "Move down half a page", remap = true })
+map("n", "<C-u>", "<C-u>zz", { desc = "Move up half a page", remap = true })
+
+map("n", "n", "nzzzv", { desc = "Find next", remap = true })
+map("n", "n", "Nzzzv", { desc = "Find prev", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- Terminal
+map("n", "<leader>fT", function()
+	Snacks.terminal()
+end, { desc = "Terminal (cwd)" })
---
+++ lua/config/opts.lua
@@ -0,0 +1,41 @@
+vim.opt.nu = true
+
+vim.wo.number = true
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "python", "lua", "yaml" },
+	callback = function()
+		vim.bo.tabstop = 4
+		vim.bo.shiftwidth = 4
+		vim.bo.expandtab = true
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "javascript", "typescript", "html", "css", "json" },
+	callback = function()
+		vim.bo.tabstop = 2
+		vim.bo.shiftwidth = 2
+		vim.bo.expandtab = true
+	end,
+})
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.wrap = false
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
